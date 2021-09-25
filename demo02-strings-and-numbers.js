ed7c39424bde2a42f0f0cc727ba09f7eb9fdc1f2
andre <andrelaprov@gmail.com>
2021-09-24 23:17:17 -0400

    typeOf

---
+++ demo02-strings-and-numbers.js
@@ -0,0 +1,23 @@
+let friendsSalary = 1000
+let mySalary = "2999aaaa"
+let myUpdatedSalary = Number(mySalary)
+
+console.log(friendsSalary + myUpdatedSalary)
+
+console.log(
+    typeof(friendsSalary),friendsSalary
+)
+
+console.log(
+    typeof(mySalary),mySalary
+)
+
+console.log(
+    isNaN(mySalary)
+)
+
+let myString = "Hello World"
+let secondString = "Hello World 2"
+let myStringWithVars = `${myString} - ${secondString}`
+
+console.log(myStringWithVars)
